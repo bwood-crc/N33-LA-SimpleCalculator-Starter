e429f8292a0c27a13d461f867e7b269932e17473
Olivier Dufort <olivedufort@gmail.com>
2024-09-04 11:53:51 -0400

    input converter finished!

--- SimpleCalculator/InputConverter.cs
+++ SimpleCalculator/InputConverter.cs
@@ -14,13 +14,19 @@ namespace SimpleCalculator
                 {
                     input = Convert.ToDouble(argTextInput);
                     tester = 0;
-                }
+                    if (tester == 0)
+                    {
+                        break;
+                    }
+               }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Enter the number's numerical value!");
+                    Console.WriteLine("Enter a number's numerical value not something else!");
                     tester = 1;
                 }
-                Console.WriteLine("Enter ");
+                Console.WriteLine("Enter the number's numerical value: ");
+                argTextInput=Console.ReadLine();
+
             }while (tester != 0);
             return input;
         }

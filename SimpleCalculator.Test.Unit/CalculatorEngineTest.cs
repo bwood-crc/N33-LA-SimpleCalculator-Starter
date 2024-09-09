82a98ed66223a61ff0badb305be5b4b1e73104a5
Alexandre Noel <2331688@champlaincollege.qc.ca>
2024-09-09 12:55:33 -0400

    Made a working Test unit according to the changes

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using CalcTools;
+using SimpleCalculator;
 namespace SimpleCalculator.Test.Unit
+
 {
     [TestClass]
-    public class CalculatorEngineTest
+    public class CalcToolsTest
     {
-        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
 
         [TestMethod]
         public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
@@ -14,7 +15,7 @@ namespace SimpleCalculator.Test.Unit
 
             int number1 = 1;
             int number2 = 2;
-            double result = _calculatorEngine.Calculate("add", number1, number2);
+            double result = CalcUti.Calculate("add", number1, number2);
             Assert.AreEqual(3, result);
 
         }
@@ -25,8 +26,92 @@ namespace SimpleCalculator.Test.Unit
 
             int number1 = 1;
             int number2 = 2;
-            double result = _calculatorEngine.Calculate("+", number1, number2);
+            double result = CalcUti.Calculate("+", number1, number2);
             Assert.AreEqual(3, result);
+
+        }
+        [TestMethod]
+        public void SubstractsTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 1;
+            double result = CalcUti.Calculate("minus", number1, number2);
+            Assert.AreEqual(1, result);
+
+        }
+
+        [TestMethod]
+        public void SubstractsTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 1;
+            double result = CalcUti.Calculate("-", number1, number2);
+            Assert.AreEqual(1, result);
+
+        }
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 2;
+            double result = CalcUti.Calculate("multiply", number1, number2);
+            Assert.AreEqual(4, result);
+
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 2;
+            int number2 = 2;
+            double result = CalcUti.Calculate("*", number1, number2);
+            Assert.AreEqual(4, result);
+
+        }
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = CalcUti.Calculate("divide", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = CalcUti.Calculate("/", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+        [TestMethod]
+        public void PercentTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 100;
+            int number2 = 10;
+            double result = CalcUti.Calculate("percent", number1, number2);
+            Assert.AreEqual(10, result);
+
+        }
+
+        [TestMethod]
+        public void PercentTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 100;
+            int number2 = 10;
+            double result = CalcUti.Calculate("%", number1, number2);
+            Assert.AreEqual(10, result);
 
         }
     }
--- SimpleCalculator.Test.Unit/InputConverterTest.cs
+++ SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using InputConverter;
 namespace SimpleCalculator.Test.Unit
 {
     [TestClass]
     public class InputConverterTest
     {
-        private readonly InputConverter _inputConverter = new InputConverter();
 
         [TestMethod]
         public void ConvertsValidStringInputIntoDouble()
         {
             string inputNumber = "5";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+            double convertedNumber = Converter.ConvertInputToNumeric(inputNumber);
             Assert.AreEqual(5, convertedNumber);
         }
 
@@ -21,8 +20,34 @@ namespace SimpleCalculator.Test.Unit
         public void FailsToConvertsInvalidStringInputIntoDouble()
         {
             string inputNumber = "*";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+            double convertedNumber = Converter.ConvertInputToNumeric(inputNumber);
             Assert.AreEqual(2, convertedNumber);
         }
-    }
+        [TestMethod]
+        public void ValidInputTest() {
+            string inputNumber = "";
+            bool test = Converter.ValidInput(inputNumber);
+            Assert.AreEqual(false, test);
+        
+        
+        }
+        [TestMethod]
+        public void ValidatesOperationString() {
+            string operation = "+";
+            bool ret = Converter.operationValidator(operation);
+            Assert.AreEqual(true, ret);
+        
+        }
+        [TestMethod]
+        public void InvalidatesOperationNumber()
+        {
+            string operation = "reuthoagiy";
+            bool ret = Converter.operationValidator(operation);
+            Assert.AreEqual(false, ret);
+
+
+
+
+        }
+        }
 }

fe7c59c29dc57bb38d0acf214604073a9d37b876
Webtx <150209138+Webtx@users.noreply.github.com>
2024-09-15 21:46:58 -0400

    Add files via upload
    calculator engine dll class library seperated from the curreny main projectttt

---
+++ CalculatorEngine/CalculatorEngine/CalculatorEngine.cs
@@ -0,0 +1,40 @@
+﻿namespace CalculatorEngine
+{
+    public class CalculatorEngine
+    {
+     
+            public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+            {
+                double result = 0;
+
+                if (argOperation == "+" || argOperation.ToLower() == "add")
+                {
+                    result = argFirstNumber + argSecondNumber;
+                }
+                else if (argOperation == "-" || argOperation.ToLower() == "subtract")
+                {
+                    result = argFirstNumber - argSecondNumber;
+                }
+                else if (argOperation == "*" || argOperation.ToLower() == "multiply")
+                {
+                    result = argFirstNumber * argSecondNumber;
+                }
+                else if (argOperation == "/" || argOperation.ToLower() == "divide")
+                {
+                    if (argSecondNumber == 0)
+                    {
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    }
+                    result = argFirstNumber / argSecondNumber;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid operation. Only +, -, *, / are supported");
+                }
+
+                return result;
+            }
+        }
+    }
+
+

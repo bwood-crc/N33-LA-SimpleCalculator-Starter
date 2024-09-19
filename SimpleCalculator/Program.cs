0c158bd8ac764a45974ea711b8cefb4502a8e9cf
natha <natha@GREY_KNIGHT>
2024-09-18 21:31:13 -0400

    string

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace SimpleCalculator
 {
@@ -8,25 +10,52 @@ namespace SimpleCalculator
         {
             try
             {
-                // Class to convert user input
-                InputConverter inputConverter = new InputConverter();
 
-                // Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
+                //String operator1 = "+";
 
-                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                string operation = Console.ReadLine();
+                //Console.WriteLine(firstNumber + operator1);
 
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine(result);
 
+                //// Class to convert user input
+                //InputConverter inputConverter = new InputConverter();
+
+                //// Class to perform actual calculations
+                //CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+
+
+                //    double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                //while (!(firstNumber.Equals(double)))
+                //{
+                //    firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                //}
+                //double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                //string operation = Console.ReadLine();
+
+                //double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+
+                double firstNumber = 10;
+                double secondNumber = 20;
+                string operator1 = "+";
+                double result = firstNumber + secondNumber;
+
+                StringBuilder stringBuilder = new StringBuilder($"{firstNumber} {operator1} {secondNumber} is equal to: {result}");
+
+                Console.WriteLine(stringBuilder.ToString());
+
+               
             } catch (Exception ex)
             {
                 // Normally, we'd log this error to a file.
                 Console.WriteLine(ex.Message);
+
+
+                
             }
+
+          
 
         }
     }
